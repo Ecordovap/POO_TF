e78a752e2dd8afdbda0b8d00df9abc9cc5a39752
lpinedo21 <104523619+lpinedo21@users.noreply.github.com>
2022-04-27 20:41:41 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
-package Grupo3;
+package CalculadoraPlanilla;
 
-public class Ejecutora {
+public class Main {
     public static void main(String[] args) {
         Planilla listaPlanilla = new Planilla();
-        listaPlanilla.agregarEmpleado(new Emprendedor(12345678,"Camila Galarreta", "Lima", 12345677, true, 400,
-                200, 350, "AB", 4, 2022,29));
+        try {
+            listaPlanilla.agregarEmpleado(new Emprendedor(76534586, "Pedro Sanchez", "Lima", 76534586, true, 4000, 2000, 3500, 4, 2022, 29));
+            listaPlanilla.agregarEmpleado(new Emprendedor(34577654, "Marcos Carrasco", "Lima", 76534586, true, 40, 10, 10, 4, 2022, 29));
+            listaPlanilla.agregarEmpleado(new PDV(34577654, "Jaime Hurtado", "Ica", 76534586, true, 30, 400, 20, true));
+
+        }
+        catch (DNIRepetido dniRep) {
+            System.out.println(dniRep.getMessage());
+        }
 
         listaPlanilla.imprimirPlanilla();
     }
