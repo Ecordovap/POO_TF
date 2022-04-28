4f315acb47a9132a547ef767e537ce245ea92d7f
lpinedo21 <104523619+lpinedo21@users.noreply.github.com>
2022-04-27 20:41:34 -0500

    Add files via upload

--- Emprendedor.java
+++ Emprendedor.java
@@ -1,74 +1,73 @@
-package ProyectoGrupal;
+package CalculadoraPlanilla;
 
 import java.time.LocalDate;
 
-public class Emprendedor extends Empleado {
+public class Emprendedor extends Empleado{
     private String escala;
     private int mesCreacion;
-    private int añocreacion;
+    private int añoCreacion;
     private int diasActivos;
 
-    public Emprendedor(int dni, String nombreCompleto, String departamento, int dniLider, boolean esLider, int volumenPP, int volumenSS, int ur, String escala, int mesCreacion, int añocreacion, int diasActivos) {
-        super(dni, nombreCompleto, departamento, dniLider, esLider, volumenPP, volumenSS, ur);
-        this.escala = escala;
+
+    public Emprendedor(int DNI, String nombreCompleto, String departamento, int DNILider, boolean esLider,
+                       int volumenPP, int volumenSS, int UR,  int mesCreacion, int añoCreacion, int diasActivos) {
+        super(DNI, nombreCompleto, departamento, DNILider, esLider, volumenPP, volumenSS, UR);
+        calcularEscala();
         this.mesCreacion = mesCreacion;
-        this.añocreacion = añocreacion;
+        this.añoCreacion = añoCreacion;
         this.diasActivos = diasActivos;
     }
 
-    //public String getEscala() {
-        //return escala;
-    //}
-
-    public String calcularEscala(){
+    public void calcularEscala(){
         LocalDate fechaDeHoy = LocalDate.now();
-        if (getVolumenPP() >= 207 && getUr() >= 122 && getVolumenSS() >= 34)
-            return "E5";
-        else if (getVolumenPP() >= 207 && getUr() >= 114 && getVolumenSS() >= 30)
-            return "E4";
-        else if(getVolumenPP() >= 188 && getUr() >= 103 && getVolumenSS() >= 24)
-            return "E3";
-        else if(getVolumenPP() >= 147 && getUr() >= 81 && getVolumenSS() >= 18)
-            return "E2";
-        else if(getVolumenPP() >= 106 && getUr() >= 58 && getVolumenSS() >= 14)
-            return "E1";
-        else if(getVolumenPP() >= 63 && getUr() >= 35 && getVolumenSS() >= 12 &&
-                fechaDeHoy.getYear() == añocreacion && fechaDeHoy.getMonthValue()==mesCreacion) //No puede tener E0 un código antiguo
-            return "E0";
-        else    return "Sin Escala";
+        if(getVolumenPP() >= 222 && getUR() >= 122 && getVolumenSS() >= 34)
+            escala = "E5";
+        else if(getVolumenPP() >= 207 && getUR() >= 114 && getVolumenSS() >= 30)
+            escala = "E4";
+        else if(getVolumenPP() >= 188 && getUR() >= 103 && getVolumenSS() >= 24)
+            escala = "E3";
+        else if(getVolumenPP() >= 147 && getUR() >= 81 && getVolumenSS() >= 18)
+            escala = "E2";
+        else if(getVolumenPP() >= 106 && getUR() >= 58 && getVolumenSS() >= 14)
+            escala = "E1";
+        else if(getVolumenPP() >= 63 && getUR() >= 35 && getVolumenSS() >= 12 &&
+                fechaDeHoy.getYear()==añoCreacion && fechaDeHoy.getMonthValue()==mesCreacion) //No puede tener E0 un código antiguo
+            escala =  "E0";
+        else
+            escala = "Sin Escala";
     }
 
     public double calcularBonoURM1(){
-        double porcURM1 = getUr()/getVolumenPP();
-        if (porcURM1 >= 0.38)
-            return getUr()*3;
-        else return  0;
+        double porcURM1 = getUR()/getVolumenPP();
+        if(porcURM1 >= 0.38)
+            return getUR()*3;
+        return 0;
     }
 
-    public double calcularBonoDiasActivos (){
-        if (diasActivos >= 28)
+    public double calcularBonoDiasActivos(){
+        if(diasActivos>=28)
             return 10;
-        else return 0;
+        return 0;
     }
+
 
     @Override
     public double calcularPlanilla() {
         double pagoPlanillaAux = 0;
-        switch (escala){
-            case "E5": pagoPlanillaAux = 4890;
-            case "E4": pagoPlanillaAux = 4455;
-            case "E3": pagoPlanillaAux = 3921;
-            case "E2": pagoPlanillaAux = 3067;
-            case "E1": pagoPlanillaAux = 2230;
-            case "E0": pagoPlanillaAux = 1314;
-            case "Sin Escala": pagoPlanillaAux = 0;
-        }
-        return pagoPlanillaAux = pagoPlanillaAux + calcularBonoURM1() + calcularBonoDiasActivos();
+        if (escala.equals("E5")) pagoPlanillaAux = 4890;
+        else if (escala.equals("E4")) pagoPlanillaAux = 4455;
+        else if (escala.equals("E3")) pagoPlanillaAux = 3921;
+        else if (escala.equals("E2")) pagoPlanillaAux = 3067;
+        else if (escala.equals("E1")) pagoPlanillaAux = 2230;
+        else if (escala.equals("E0")) pagoPlanillaAux = 1314;
+        else if (escala.equals("Sin escala")) pagoPlanillaAux = 0;
+
+        return pagoPlanillaAux + calcularBonoURM1() + calcularBonoDiasActivos();
     }
 
     @Override
     public void imprimirEmpleado() {
-        System.out.println("Tipo emprendedor - DNI: "+getDni()+ " , Nombre: "+getNombreCompleto()+ " , Escala: "+calcularEscala()+
-                " , Pago planilla: "+calcularPlanilla());
+        System.out.println("Tipo Emprendedor - DNI: " + getDNI() + ", Nombre: " + getNombreCompleto() + ", Escala: " + escala +
+                ", Pago planilla: " + calcularPlanilla());
     }
 }
