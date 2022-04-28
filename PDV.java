b9422137158726210fcb327412de6625cc1b712c
lpinedo21 <104523619+lpinedo21@users.noreply.github.com>
2022-04-27 20:41:49 -0500

    Add files via upload

--- PDV.java
+++ PDV.java
@@ -1,38 +1,41 @@
-package ProyectoGrupal;
+package CalculadoraPlanilla;
 
-public class Pdv extends Empleado{
+public class PDV extends Empleado{
     private String clasificacion;
     private boolean tieneEquipo;
 
-    public Pdv(int dni, String nombreCompleto, String departamento, int dniLider, boolean esLider, int volumenPP, int volumenSS, int ur, String clasificacion, boolean tieneEquipo) {
-        super(dni, nombreCompleto, departamento, dniLider, esLider, volumenPP, volumenSS, ur);
-        this.clasificacion = clasificacion;
+    public PDV(int DNI, String nombreCompleto, String departamento, int DNILider, boolean esLider,
+               int volumenPP, int volumenSS, int UR, boolean tieneEquipo) {
+        super(DNI, nombreCompleto, departamento, DNILider, esLider, volumenPP, volumenSS, UR);
+        this.clasificacion = calcularClasificacion();
         this.tieneEquipo = tieneEquipo;
     }
+
     public String calcularClasificacion(){
-        if (getVolumenPP() >= 7 && getVolumenSS() >= 20)
+        if(getVolumenPP() >= 7 && getVolumenSS() >= 20)
             return "Oro";
-        else if (getVolumenPP() >= 5 && getVolumenSS() >= 15)
+        else if(getVolumenPP() >= 5 && getVolumenSS() >= 15)
             return "Plata";
-        else return "Bonce";
+        return "Bronce";
     }
 
     @Override
     public double calcularPlanilla() {
         double auxPagoPlanilla = 0;
-        switch (clasificacion){
-            case "Oro": auxPagoPlanilla = 1200;
-            case "Plata": auxPagoPlanilla = 1000;
-            case "Bronce": auxPagoPlanilla = 800;
-        }
-        if (tieneEquipo) auxPagoPlanilla = auxPagoPlanilla + 50;
-        if (getUr()/getVolumenPP() >= 0.55) auxPagoPlanilla = auxPagoPlanilla + 100;
-        return auxPagoPlanilla;
+        if(clasificacion.equals("Oro")) auxPagoPlanilla = 1200;
+        else if(clasificacion.equals("Plata")) auxPagoPlanilla = 1000;
+        else if(clasificacion.equals("Bronce")) auxPagoPlanilla = 800;
+
+        if(tieneEquipo) auxPagoPlanilla = auxPagoPlanilla + 50;
+
+        if(getUR()/getVolumenPP() >= 0.55) auxPagoPlanilla = auxPagoPlanilla + 100;
+
+        return  auxPagoPlanilla;
     }
 
     @Override
     public void imprimirEmpleado() {
-        System.out.println("Tipo Pdv - DNI: "+getDni()+ " , Nombre: "+getNombreCompleto()+ " , Clasificacion: "+calcularClasificacion()+
-                " , Pago planilla: "+calcularPlanilla());
+        System.out.println("Tipo PDV - DNI: " + getDNI() + ", Nombre: " + getNombreCompleto() + ", Clasificacion: " + calcularClasificacion() +
+                ", Pago planilla: " + calcularPlanilla());
     }
 }
