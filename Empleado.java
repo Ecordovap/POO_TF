daa7c09ae070746504372df646dda99539ea2c73
Ecordovap <95503412+Ecordovap@users.noreply.github.com>
2022-04-27 20:30:57 -0500

    edit1
    Se actualizó código

--- Empleado.java
+++ Empleado.java
@@ -1,14 +1,58 @@
 package ProyectoGrupal;
 
-public class Empleado  extends  Persona{
-    private String tipo;
-    private int DNILider;
-    private String clase;
+public abstract class Empleado {
+    private int dni;
+    private String nombreCompleto;
+    private String departamento;
+    private int dniLider;
+    private boolean esLider;
+    private int volumenPP;
+    private int volumenSS;
+    private int ur;
 
-    public Empleado(int DNI, String nombres, String apellidos, String direccion, String tipo, int DNILider, String clase) {
-        super(DNI, nombres, apellidos, direccion);
-        this.tipo = tipo;
-        this.DNILider = DNILider;
-        this.clase = clase;
+    public Empleado(int dni, String nombreCompleto, String departamento, int dniLider, boolean esLider, int volumenPP, int volumenSS, int ur) {
+        this.dni = dni;
+        this.nombreCompleto = nombreCompleto;
+        this.departamento = departamento;
+        this.dniLider = dniLider;
+        this.esLider = esLider;
+        this.volumenPP = volumenPP;
+        this.volumenSS = volumenSS;
+        this.ur = ur;
     }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public int getDniLider() {
+        return dniLider;
+    }
+
+    public boolean isEsLider() {
+        return esLider;
+    }
+
+    public int getVolumenPP() {
+        return volumenPP;
+    }
+
+    public int getVolumenSS() {
+        return volumenSS;
+    }
+
+    public int getUr() {
+        return ur;
+    }
+
+    public abstract double calcularPlanilla();
+    public abstract void imprimirEmpleado();
 }
