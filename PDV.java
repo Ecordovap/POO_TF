ab123c713be437ec89af94fb8a795efa8de9435b
Ecordovap <95503412+Ecordovap@users.noreply.github.com>
2022-04-27 20:36:26 -0500

    Ed
    Se actualizó código.

--- PDV.java
+++ PDV.java
@@ -1,13 +1,38 @@
 package ProyectoGrupal;
 
-public class PDV extends Empleado{
-    private boolean equipo;
+public class Pdv extends Empleado{
     private String clasificacion;
+    private boolean tieneEquipo;
 
-    public PDV(int DNI, String nombres, String apellidos, String direccion, String tipo,
-               int DNILider, String clase, boolean equipo, String clasificacion) {
-        super(DNI, nombres, apellidos, direccion, tipo, DNILider, clase);
-        this.equipo = equipo;
+    public Pdv(int dni, String nombreCompleto, String departamento, int dniLider, boolean esLider, int volumenPP, int volumenSS, int ur, String clasificacion, boolean tieneEquipo) {
+        super(dni, nombreCompleto, departamento, dniLider, esLider, volumenPP, volumenSS, ur);
         this.clasificacion = clasificacion;
+        this.tieneEquipo = tieneEquipo;
+    }
+    public String calcularClasificacion(){
+        if (getVolumenPP() >= 7 && getVolumenSS() >= 20)
+            return "Oro";
+        else if (getVolumenPP() >= 5 && getVolumenSS() >= 15)
+            return "Plata";
+        else return "Bonce";
+    }
+
+    @Override
+    public double calcularPlanilla() {
+        double auxPagoPlanilla = 0;
+        switch (clasificacion){
+            case "Oro": auxPagoPlanilla = 1200;
+            case "Plata": auxPagoPlanilla = 1000;
+            case "Bronce": auxPagoPlanilla = 800;
+        }
+        if (tieneEquipo) auxPagoPlanilla = auxPagoPlanilla + 50;
+        if (getUr()/getVolumenPP() >= 0.55) auxPagoPlanilla = auxPagoPlanilla + 100;
+        return auxPagoPlanilla;
+    }
+
+    @Override
+    public void imprimirEmpleado() {
+        System.out.println("Tipo Pdv - DNI: "+getDni()+ " , Nombre: "+getNombreCompleto()+ " , Clasificacion: "+calcularClasificacion()+
+                " , Pago planilla: "+calcularPlanilla());
     }
 }
